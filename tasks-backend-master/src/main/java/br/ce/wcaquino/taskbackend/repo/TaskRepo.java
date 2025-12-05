f2aee308c6e573a324cbc624e76c0ae6181d10af
Leonardo Samuel <leonardosmsilva2004@gmail.com>
2025-12-05 09:14:16 -0300

    atualiza repository

--- tasks-backend-master/src/main/java/br/ce/wcaquino/taskbackend/repo/TaskRepo.java
+++ tasks-backend-master/src/main/java/br/ce/wcaquino/taskbackend/repo/TaskRepo.java
@@ -1,12 +1,13 @@
 package br.ce.wcaquino.taskbackend.repo;
 
+import br.ce.wcaquino.taskbackend.model.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
 import br.ce.wcaquino.taskbackend.model.Task;
 import java.util.List;
 
 public interface TaskRepo extends JpaRepository<Task, Long> {
 
-    List<Task> findByStatus(Task.Status status);
+    List<Task> findByStatus(Status status);
 
-    boolean existsByStatus(Task.Status status);
+    boolean existsByStatus(Status status);
 }
