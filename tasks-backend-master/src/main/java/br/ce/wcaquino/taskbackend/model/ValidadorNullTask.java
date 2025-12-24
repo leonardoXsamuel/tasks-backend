411eccfff942da5342391598fe6c1e266a5fac16
Leonardo Samuel <leonardosmsilva2004@gmail.com>
2025-12-24 13:55:59 -0300

    feat: adicionando um validador para cada atributo do model Task

--- tasks-backend-master/src/main/java/br/ce/wcaquino/taskbackend/model/ValidadorNullTask.java
+++ tasks-backend-master/src/main/java/br/ce/wcaquino/taskbackend/model/ValidadorNullTask.java
@@ -1,2 +1,25 @@
-package br.ce.wcaquino.taskbackend.model;public class validadorNullTask {
+package br.ce.wcaquino.taskbackend.model;
+
+import br.ce.wcaquino.taskbackend.dto.TaskCreateDTO;
+
+import java.time.LocalDateTime;
+
+public class ValidadorNullTask {
+
+    public void validar(TaskCreateDTO dto) {
+
+        if (dto.nome() == null || dto.nome().isBlank()) {
+            throw new IllegalArgumentException("Nome é obrigatório");
+        }
+        if (dto.descricao() == null || dto.descricao().isBlank()) {
+            throw new IllegalArgumentException("Descrição é obrigatória");
+        }
+        if (dto.dataConclusao() == null || dto.dataConclusao().isBefore(LocalDateTime.now())) {
+            throw new IllegalArgumentException("Data inválida");
+        }
+        if (dto.status() == null) {
+            throw new IllegalArgumentException("Status inválido");
+        }
+
+    }
 }
