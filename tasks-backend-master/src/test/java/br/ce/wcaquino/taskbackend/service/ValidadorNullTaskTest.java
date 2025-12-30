b20322ef5ed9d28a318f9257f6561f84b357ac8b
Leonardo Samuel <leonardosmsilva2004@gmail.com>
2025-12-30 19:54:37 -0300

    test: teste de dto passando na validação e teste de dto nao passando na validação

--- tasks-backend-master/src/test/java/br/ce/wcaquino/taskbackend/service/ValidadorNullTaskTest.java
+++ tasks-backend-master/src/test/java/br/ce/wcaquino/taskbackend/service/ValidadorNullTaskTest.java
@@ -1,4 +1,42 @@
-import static org.junit.jupiter.api.Assertions.*;
+package br.ce.wcaquino.taskbackend.service;
+
+import br.ce.wcaquino.taskbackend.dto.TaskCreateDTO;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.time.LocalDateTime;
+
+import static br.ce.wcaquino.taskbackend.model.Status.ATRASADA;
+import static br.ce.wcaquino.taskbackend.model.Status.CONCLUIDA;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+@ExtendWith(MockitoExtension.class)
 class ValidadorNullTaskTest {
-  
+
+    @InjectMocks
+    private ValidadorNullTask validadorNullTask;
+
+    @Test
+    @DisplayName("DTO passou na validação.")
+    void validarSuccess() {
+        // ARRANGE
+        TaskCreateDTO dto = new TaskCreateDTO("task", LocalDateTime.now().plusDays(2), "descricao", CONCLUIDA);
+
+        // ACT + ASSERT
+        Assertions.assertDoesNotThrow(() -> validadorNullTask.validarCampos(dto.nome(), dto.descricao(), dto.dataConclusao(), dto.status()));
+    }
+
+    @Test
+    @DisplayName("Deve lançar erro quando DTO inválido")
+    void validarError() {
+        // ARRANGE
+        TaskCreateDTO dto = new TaskCreateDTO("", LocalDateTime.now().minusDays(2), "", ATRASADA);
+
+        // ACT + ASSERT
+        assertThrows(IllegalArgumentException.class, () -> validadorNullTask.validarCampos(dto.nome(), dto.descricao(), dto.dataConclusao(), dto.status()));
+    }
 }
