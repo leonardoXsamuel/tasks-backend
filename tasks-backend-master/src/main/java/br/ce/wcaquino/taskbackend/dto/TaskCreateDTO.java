f386d2aa4316ad758f87aa255cd85a8be5eb8bf7
Leonardo Samuel <leonardosmsilva2004@gmail.com>
2026-01-02 00:48:41 -0300

    feat: adiciona validações nos DTOs de task

--- tasks-backend-master/src/main/java/br/ce/wcaquino/taskbackend/dto/TaskCreateDTO.java
+++ tasks-backend-master/src/main/java/br/ce/wcaquino/taskbackend/dto/TaskCreateDTO.java
@@ -1,7 +1,18 @@
 package br.ce.wcaquino.taskbackend.dto;
 
+import jakarta.validation.constraints.*;
 import java.time.LocalDateTime;
 
-public record TaskCreateDTO(String nome, LocalDateTime dataConclusao, String descricao,
-                            br.ce.wcaquino.taskbackend. model.Status status) {
-}
+public record TaskCreateDTO(
+        @NotBlank(message = "nome da tarefa deve ser preenchido")
+        String nome,
+
+        @FutureOrPresent(message = "A data da tarefa deve ser no presente ou no futuro.")
+        LocalDateTime dataConclusao,
+
+        @NotBlank(message = "Descrição da tarefa deve ser preenchida.")
+        String descricao,
+
+        @NotNull(message = "O status deve ser: PENDENTE, CONCLUIDA ou ATRASADA.")
+        br.ce.wcaquino.taskbackend. model.Status status
+) { }
--- tasks-backend-master/src/main/java/br/ce/wcaquino/taskbackend/dto/TaskUpdateDTO.java
+++ tasks-backend-master/src/main/java/br/ce/wcaquino/taskbackend/dto/TaskUpdateDTO.java
@@ -1,9 +1,22 @@
 package br.ce.wcaquino.taskbackend.dto;
 
-import br.ce.wcaquino.taskbackend.model.Status;
+import jakarta.validation.constraints.FutureOrPresent;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 import java.time.LocalDateTime;
 
-public record TaskUpdateDTO(String nome, LocalDateTime dataConclusao, String descricao, Status status) {
+public record TaskUpdateDTO(
+        @NotBlank(message = "nome da tarefa deve ser preenchido")
+        String nome,
+
+        @FutureOrPresent(message = "A data da tarefa deve ser no presente ou no futuro.")
+        LocalDateTime dataConclusao,
+
+        @NotBlank(message = "Descrição da tarefa deve ser preenchida.")
+        String descricao,
+
+        @NotNull(message = "O status deve ser: PENDENTE, CONCLUIDA ou ATRASADA.")
+        br.ce.wcaquino.taskbackend. model.Status status) {
 
 }
